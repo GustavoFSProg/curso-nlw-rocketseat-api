1d3b7718a5af9c65e28675d262a555dafa560f52
Gustavo <gustavosohne38@gmail.com>
2020-08-13 17:57:53 -0300

    eee

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,14 +5,15 @@ dotenv.config()
 
 const db = knex({
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  // connection: process.env.DATABASE_URL,
 
-  // connection: {
-  //   host: process.env.PG_HOST,
-  //   user: process.env.PG_USER,
-  //   password: process.env.PG_PASSWORD,
-  //   database: process.env.PG_DATABASE,
-  // },
+  connection: {
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+  },
+
   useNullAsDefault: true,
 })
 
