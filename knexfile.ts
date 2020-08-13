610c5d89c6a7e4f1ee56e23d8997dbab2713ac8e
Gustavo <gustavosohne38@gmail.com>
2020-08-13 17:44:10 -0300

    endin

--- knexfile.ts
+++ knexfile.ts
@@ -5,12 +5,13 @@ dotenv.config()
 
 module.exports = {
   client: 'pg',
-  connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  },
+  connection: process.env.DATABASE_URL,
+  // connection: {
+  //   host: process.env.PG_HOST,
+  //   user: process.env.PG_USER,
+  //   password: process.env.PG_PASSWORD,
+  //   database: process.env.PG_DATABASE,
+  // },
 
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,12 +5,14 @@ dotenv.config()
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  },
+  connection: process.env.DATABASE_URL,
+
+  // connection: {
+  //   host: process.env.PG_HOST,
+  //   user: process.env.PG_USER,
+  //   password: process.env.PG_PASSWORD,
+  //   database: process.env.PG_DATABASE,
+  // },
   useNullAsDefault: true,
 })
 
